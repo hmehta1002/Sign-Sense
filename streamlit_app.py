af9485efabbe53f55c7d7dfa0673d66fa5350301
hmehta1002 <hmehta1002@gmail.com>
2025-12-08 13:42:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,9 @@ import os
 import sys
 import streamlit as st
 
-# Make sure Python can see the src/ folder
+# Add src/ to Python path
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 SRC_DIR = os.path.join(ROOT_DIR, "src")
-
 if SRC_DIR not in sys.path:
     sys.path.insert(0, SRC_DIR)
 
