d4feef0fe74513f7979bd82ae7ec0b394421221d
hmehta1002 <hmehta1002@gmail.com>
2025-12-10 01:33:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,9 @@
 import sys
 from pathlib import Path
 
-# Ensure Streamlit can find src/
-BASE_DIR = Path(__file__).resolve().parent / "src"
-sys.path.append(str(BASE_DIR))
+BASE = Path(__file__).resolve().parent / "src"
+sys.path.append(str(BASE))
 
-# Import your main app file
 import app
 
 if __name__ == "__main__":
